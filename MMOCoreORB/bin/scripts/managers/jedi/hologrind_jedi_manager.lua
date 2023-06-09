778da2465a17ecff8f11d3a91c9959e743daaf41
TrigsC <trigsc@gmail.com>
2023-06-08 20:22:38 -0700

    test buff

--- MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
+++ MMOCoreORB/bin/scripts/managers/jedi/hologrind_jedi_manager.lua
@@ -181,6 +181,7 @@ function HologrindJediManager:onPlayerLoggedIn(pCreatureObject)
 	if (pCreatureObject == nil) then
 		return
 	end
+	CreatureObject(pCreatureObject):enhanceCharacter()
 
 	self:checkIfProgressedToJedi(pCreatureObject)
 	self:registerObservers(pCreatureObject)
