a1017a87cd17b1f1aaae9b171d4ae4ec6f75e35d
TrigsC <trigsc@gmail.com>
2023-06-27 12:15:28 -0700

    wrong dir

--- MMOCoreORB/bin/scripts/managers/jedi/my_python.py
+++ MMOCoreORB/bin/scripts/managers/jedi/my_python.py
@@ -19,7 +19,8 @@ def main():
 
     # Enable to save to disk & reuse the model (for repeated queries on the same data)
     PERSIST = True
-    persist_dir = "D:\\DedaFix\\SWGSolo\\MMOCoreORB\\bin\\scripts\\persist"
+    #persist_dir = "D:\\DedaFix\\SWGSolo\\MMOCoreORB\\bin\\scripts\\persist"
+    persist_dir = "/home/swgemu/Core3/MMOCoreORB/bin/scripts/persist"
     #if PERSIST and os.path.exists("/home/swgemu/Core3/MMOCoreORB/bin/scripts/persist"):
     #if PERSIST and os.path.exists("D:\\DedaFix\\SWGSolo\\MMOCoreORB\\bin\\conf\\persist"):
     if PERSIST and os.path.exists(persist_dir):
@@ -28,8 +29,8 @@ def main():
       index = VectorStoreIndexWrapper(vectorstore=vectorstore)
     else:
       #loader = TextLoader("data/data.txt") # Use this line if you only need data.txt
-      #loader = DirectoryLoader("/home/swgemu/Core3/MMOCoreORB/bin/scripts/data_files/")
-      loader = DirectoryLoader("D:\\DedaFix\\SWGSolo\\MMOCoreORB\\bin\\scripts\\data_files\\")
+      loader = DirectoryLoader("/home/swgemu/Core3/MMOCoreORB/bin/scripts/data_files/")
+      #loader = DirectoryLoader("D:\\DedaFix\\SWGSolo\\MMOCoreORB\\bin\\scripts\\data_files\\")
       if PERSIST:
         index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory":persist_dir}).from_loaders([loader])
       else:
