799dd5b3f527b98d09b7a5636149058c2d8945d4
TrigsC <trigsc@gmail.com>
2023-07-12 11:24:47 -0700

    twohand ls gen5 test2

--- MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_gen5.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_gen5.lua
@@ -139,6 +139,4 @@ object_weapon_melee_2h_sword_crafted_saber_sword_lightsaber_two_handed_gen5 = ob
 	experimentalCombineType = {0, 0, 1, 1, 1, 1, 1, 1, 1, 1},
 }
 
-}
-
 ObjectTemplates:addTemplate(object_weapon_melee_2h_sword_crafted_saber_sword_lightsaber_two_handed_gen5, "object/weapon/melee/2h_sword/crafted_saber/sword_lightsaber_two_handed_gen5.iff")
