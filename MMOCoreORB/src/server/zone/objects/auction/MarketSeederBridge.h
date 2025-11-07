f3eb04bab76e9be91e5a9bf3199864abc6ca4ee9
TrigsC <42152053+TrigsC@users.noreply.github.com>
2025-11-07 09:17:08 -0700

    Update MarketSeederBridge.h

--- MMOCoreORB/src/server/zone/objects/auction/MarketSeederBridge.h
+++ MMOCoreORB/src/server/zone/objects/auction/MarketSeederBridge.h
@@ -8,7 +8,7 @@
 #include "server/zone/objects/scene/SceneObject.h"
 #include "server/zone/objects/creature/CreatureObject.h"
 
-class Zone;
+namespace server { namespace zone { class Zone; } }
 
 struct lua_State;
 
@@ -16,14 +16,15 @@ class MarketSeederBridge {
 public:
         static bool listOnBazaar(SceneObject* item, CreatureObject* seller, const String& planet, float x, float y, int price, int durationHours);
 
-        static CreatureObject* getSystemSeller(Zone* zone);
+        static CreatureObject* getSystemSeller(server::zone::Zone* zone);
 
         static int luaListOnBazaar(lua_State* L);
         static int luaGetSystemSeller(lua_State* L);
 
 private:
-        static CreatureObject* ensureSystemSeller(Zone* zone);
-        static SceneObject* findBazaarTerminal(Zone* zone, float x, float y, float searchRadius);
+    static CreatureObject* ensureSystemSeller(server::zone::Zone* zone);
+    static SceneObject*    findBazaarTerminal(server::zone::Zone* zone, float x, float y, float searchRadius);
 };
+
 
 #endif // MARKETSEEDERBRIDGE_H_
