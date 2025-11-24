b95cf2d2cdbc07887ebe1f17256bee79ea27d256
TrigsC <trigsc@gmail.com>
2025-11-24 12:25:44 -0700

    Adding AI Defense + test 8

--- MMOCoreORB/bin/scripts/mobile/faction/rebel/light_jedi_sentinel.lua
+++ MMOCoreORB/bin/scripts/mobile/faction/rebel/light_jedi_sentinel.lua
@@ -8,20 +8,14 @@ light_jedi_sentinel = Creature:new {
 	level = 88,
 	--chanceHit = 30,
 	statistics = {
-        -- ACCURACY (Attacking)
         attack_accuracy = 100,
-        melee_accuracy = 60,  -- Total 180 accuracy
-        
-        -- PRIMARY DEFENSE (Avoiding the hit)
-        -- Game adds Level (88) automatically. 
-        -- 88 + 40 = 128 (Hard Capped at 125).
-        dodge_attack = 40,    -- Used vs Ranged
-        block = 40,           -- Used vs Melee
-        counter_attack = 40,  -- Chance to counter-hit vs Melee
+        melee_accuracy = 60,
+		melee_defense = 55,
+        dodge_attack = 40,
+        block = 40,
+        counter_attack = 40, 
         lightsaber_toughness = 55,
 		jedi_toughness = 45,
-        -- SECONDARY DEFENSE (Mitigating the hit)
-        -- This is a flat % roll. 95 = 95% chance to reduce damage if hit.
         saber_block = 95,
     },
 	damageMin = 500,
