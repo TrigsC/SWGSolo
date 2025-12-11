e62755af057ce0c705fb9a4917a8a751253ac2a2
TrigsC <trigsc@gmail.com>
2025-12-11 08:29:30 -0700

    First Sim Player 21

--- MMOCoreORB/src/server/zone/objects/creature/simplayer/SimPlayerController.h
+++ MMOCoreORB/src/server/zone/objects/creature/simplayer/SimPlayerController.h
@@ -10,7 +10,7 @@
 
 #include "engine/core/Task.h"
 #include "engine/core/ManagedReference.h"
-#include "system/lang/ref/ReferenceCounter.h" // <--- CHANGED THIS
+#include "system/lang/Object.h"
 #include "engine/log/Logger.h"
 #include "system/util/Vector.h"
 #include "server/zone/objects/scene/WorldCoordinates.h"
@@ -33,10 +33,11 @@ public:
     void run() override; 
 };
 
-// Inherit from ReferenceCounter (for smart pointers) and Logger (for info())
-class SimPlayerController : public ReferenceCounter, public Logger { // <--- CHANGED THIS
+// Inheriting from Object gives us acquire()/release() for Smart Pointers
+class SimPlayerController : public Object, public Logger {
     ManagedReference<AiAgent*> agent;
     
+    // Member variables for the Retry Loop
     String targetResource;
     int retryCount;
 
